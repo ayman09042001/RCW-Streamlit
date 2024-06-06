458407edd4da37fe774e52237a5c37ac62be2130
ayman09042001 <AymanEddahbi@teccart.online>
2024-06-06 12:16:48 -0400

    Connexion a snowflake et affichage des données

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 import streamlit as st
+import snowflake.connector as sc
+import pandas as pd
 def main():
-    st.write("allo")
+    st.title("Demo de la connexion à Snowflake depuis streamlit")
+    try:
+        con = sc.connect(
+        account = 'kjbusla-kf63760',
+        user = 'AEDDAHBI5',
+        password = 'Ziad.akodad02'
+        )
+        cursor = con.cursor()
+        def dataPersons():
+            sql = "SELECT * FROM RCW.PERSONS.PERSONS"
+            df = cursor.execute(sql).fetchall()
+            return pd.DataFrame(df)
+        donnees = dataPersons()
+        st.write(donnees)
+    except sc.Error as e:
+        st.error(f"Error connecting to Snowflake: {e}")
+
+
 
 
 if __name__ == "__main__":
